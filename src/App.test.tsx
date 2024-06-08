45cc05807edd83865cffc7d70e13b5e703cea554
Steven Augusto Villegas Castellanos <steve@Stevens-MacBook-Pro.local>
2024-06-08 12:47:21 -0500

    app works and basic unit tests added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App tests', () => {
+  it('should be defined', () => {
+    expect(App).toBeDefined();
+  });
+
+  it('should be rendered successfully', () => {
+    render(<App />);
+
+    expect(screen.getByText('Find a city')).toBeInTheDocument();
+  });
 });
---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+export interface City {
+  country: string;
+  name: string;
+  lat: number;
+  lng: number;
+}
+
